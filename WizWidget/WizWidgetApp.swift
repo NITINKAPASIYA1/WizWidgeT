0b3fc2448ec804289c4377ebfa232a2db7ddc8ac
Nitin Kumar <kumarnitin5814@gmail.com>
2025-04-04 01:24:11 +0530

    Initial Commit

---
+++ WizWidget/WizWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizWidgetApp.swift
+//  WizWidget
+//
+//  Created by Nitin on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WizWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
